853d55e5f912f234921a108aa0a563e51115e9f8
Joeth246 <57768855+Joeth246@users.noreply.github.com>
2026-06-12 16:54:50 -0400

    Add file reporting and sorting functionality

--- utilityhandler.ps1
+++ utilityhandler.ps1
@@ -1,1 +1,24 @@
 
+$Files = Get-ChildItem
+
+$FilesCount = $Files.Count
+
+Write-Host "The total number of files: $FilesCount"
+
+$SortedFiles = $Files |
+Sort-Object Length -Descending |
+Select-Object -First 5
+
+Write-Host "The largest files are:"
+
+$SortedFiles | Select-Object Name, Length | Format-Table
+
+$Report = $Files |
+Select-Object Name, Length, Extension, LastWriteTime
+
+New-Item -ItemType Directory -Path "reports" -Force
+
+$Report |
+Export-Csv -Path "reports\FileReport.csv" 
+
+Write-Host "Report exported successfully."
